a871ab28155c7d3b92b3fac46f9630fbb34173a1
Mariia <mytsko.mariia@gmail.com>
2020-09-13 22:08:28 +0200

    change photo

--- src/posts/post.js
+++ src/posts/post.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./posts.css";
-import myFoto from "./img/my-photo.jpg";
+import myPhoto from "./img/my-photo.jpg";
 import postPhoto from "./img/val-playa.jpg";
 
-const MY_IMAGE = myFoto;
+const MY_IMAGE = myPhoto;
 const POST_IMG = postPhoto;
 
 function formatDate(date) {
@@ -15,7 +15,7 @@ function Post(props) {
       <div className="wrapper">
         <div className="userInfo">
           <div className="post-wrapper">
-            <img className="profilePhoto" src={MY_IMAGE} alt={myFoto} />
+            <img className="profilePhoto" src={MY_IMAGE} alt={myPhoto} />
             {props.author.name}
             <span className="nickname-date">
               {props.author.nickname} . {formatDate(props.date)}
@@ -29,13 +29,5 @@ function Post(props) {
     </>
   );
 }
-const post = {
-  date: new Date(),
-  text: `Today was a good day to try to surf`,
-  author: {
-    name: "Mariia Mytsko",
-    nickname: "@Mariia_Myt",
-  },
-};
 
 export default Post;
